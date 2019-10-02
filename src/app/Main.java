957c30ad4151b749f67cf5515a53e09b08a8f2d9
Youssef <yousseftarek@live.com>
2019-10-02 11:12:57 +0200

    ^ search tree node

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,41 +1,59 @@
 package app;
 
+import java.util.ArrayList;
+
 public class Main {
 
-    /**
-     * The method uses search to try to formulate a winning plan with the
-     * corresponding cost and number of nodes explored
-     * 
-     * @param grid      is a string representing the grid to perform the search on
-     * @param strategy  is a string indicating the search strategy to be applied
-     *                  [BF, DF, ID, UC, GRi, ASi]
-     * @param visualize is a boolean parameter which, when set to true, results in
-     *                  your pro- gram’s side-effecting a visual presentation of the
-     *                  grid as it undergoes the diffe- rent steps of the discovered
-     *                  solution
-     * @return String representing the formulated plan with a cost and number of
-     *         nodes in format: plan;cost;nodes
-     */
-    public static String solve(String grid, String strategy, Boolean visualize) {
+	/**
+	 * The method uses search to try to formulate a winning plan with the
+	 * corresponding cost and number of nodes explored
+	 * 
+	 * @param grid      is a string representing the grid to perform the search on
+	 * @param strategy  is a string indicating the search strategy to be applied
+	 *                  [BF, DF, ID, UC, GRi, ASi]
+	 * @param visualize is a boolean parameter which, when set to true, results in
+	 *                  your program's side-effecting a visual presentation of the
+	 *                  grid as it undergoes the different steps of the discovered
+	 *                  solution
+	 * @return String representing the formulated plan with a cost and number of
+	 *         nodes in format: plan;cost;nodes
+	 */
+	public static String solve(String grid, String strategy, Boolean visualize) {
 
-        String plan = "";
-        String cost = "";
-        String nodes = "";
+		String plan = "";
+		String cost = "";
+		String nodes = "";
 
-        if (plan == null)
-            return "There is no solution";
+		if (plan == null)
+			return "There is no solution";
 
-        if (visualize) {
-            // TODO: run the visualization module
-        }
+		if (visualize) {
+			// TODO: run the visualization module
+		}
 
-        String formulatedPlan = plan + ';' + cost + ';' + nodes;
-        return formulatedPlan;
-    }
+		String formulatedPlan = plan + ';' + cost + ';' + nodes;
+		return formulatedPlan;
+	}
 
-    public static void main(String[] args) throws Exception {
-        String grid = "5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3";
-        String strategy = "BF";
-        solve(grid, strategy, true);
-    }
+	public ArrayList<SearchTreeNode> queueing(SearchTreeNode node) {
+		return null;
+	}
+
+	public void searchBreadthFirst(SearchTreeNode node) {
+		
+	}
+	
+	public void searchDepthFirst(SearchTreeNode node) {
+		
+	}
+	
+public void searchUniFormCost(SearchTreeNode node) {
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+		String grid = "5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3";
+		String strategy = "BF";
+		solve(grid, strategy, true);
+	}
 }
--- src/app/SearchTreeNode.java
+++ src/app/SearchTreeNode.java
@@ -5,17 +5,38 @@ package app;
  */
 
 public class SearchTreeNode {
-    public State state;
-    public SearchTreeNode parent;
-    public String leadingOperator;
-    public int cost;
-    public int depth;
+	public State state;
+	public SearchTreeNode parent;
+	public String leadingOperator;
+	public int cost;
+	public int depth;
 
-    public SearchTreeNode(State state, SearchTreeNode parent, String leadingOperator, int cost, int depth) {
-        this.state = state;
-        this.parent = parent;
-        this.leadingOperator = leadingOperator;
-        this.cost = cost;
-        this.depth = depth;
-    }
+	public SearchTreeNode(State state, SearchTreeNode parent, String leadingOperator, int cost, int depth) {
+		this.state = state;
+		this.parent = parent;
+		this.leadingOperator = leadingOperator;
+		this.cost = cost;
+		this.depth = depth;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public SearchTreeNode getParent() {
+		return parent;
+	}
+
+	public String getLeadingOperator() {
+		return leadingOperator;
+	}
+
+	public int getCost() {
+		return cost;
+	}
+
+	public int getDepth() {
+		return depth;
+	}
+
 }
