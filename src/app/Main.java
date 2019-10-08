abb3cd569072d5bd69b2b973947e5fec216895d4
Youssef <yousseftarek@live.com>
2019-10-08 22:39:33 +0200

    switched to DF

--- src/app/Main.java
+++ src/app/Main.java
@@ -45,7 +45,7 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		String grid = "5,5;1,2;3,1;0,2,1,1,2,1,2,2,4,0,4,1;0,3,3,0,3,2,3,4,4,3";
 //		String grid = "3,3;0,0;1,1;0,1,2,0;1,0";
-		SearchingAlgorithms strategy = SearchingAlgorithms.BF;
+		SearchingAlgorithms strategy = SearchingAlgorithms.DF;
 		String solution = solve(grid, strategy, true);
 		System.out.println(solution);
 	}
